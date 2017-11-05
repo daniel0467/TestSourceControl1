5afd22162ff6bbccc043e6cb16e2bef580beffac
Daniel <daniel@dynamic.wline.6rd.res.cust.swisscom.ch>
2017-11-05 13:44:47 +0100

    Plus 1

--- TestSourceControl/ViewController.swift
+++ TestSourceControl/ViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 0
+    
     @IBOutlet weak var Textbox: UITextField!
     @IBOutlet weak var Label: UILabel!
     @IBAction func Exit(_ sender: UIButton) {
-        Textbox.text = "Exit gedrückt"
+        counter += 1
+        Textbox.text = String(counter)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Textbox.text = String(counter)
     }
 
     override func didReceiveMemoryWarning() {
